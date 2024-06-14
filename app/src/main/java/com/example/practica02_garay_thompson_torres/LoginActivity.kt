1c656c69512c4a90e74de64edb9162b10abebb7a
ArianGaray <ariangaray.1402@gmail.com>
2024-06-14 10:53:20 -0500

    login

---
+++ app/src/main/java/com/example/practica02_garay_thompson_torres/LoginActivity.kt
@@ -0,0 +1,53 @@
+package com.example.practica02_garay_thompson_torres
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login) // Aquí se cambió a activity_login
+
+        auth = FirebaseAuth.getInstance()
+
+        val emailEditText: EditText = findViewById(R.id.etCorreo)
+        val passwordEditText: EditText = findViewById(R.id.etContra)
+        val loginButton: Button = findViewById(R.id.btLogin)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                signInWithEmailAndPassword(email, password)
+            } else {
+                Toast.makeText(this, "Please enter email and password.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signInWithEmailAndPassword(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    Toast.makeText(this, "Authentication successful!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed: ${task.exception?.message}",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
